bcc7e824a81d898efae08a302fe49d839e70b9ec
jacksonPAO <jackson970406@gmail.com>
2019-01-31 02:33:21 -0500

    DevOps Rpao 1/31

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 80:5000 --name flask-devops -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/rpao_webtest:latest
+docker run -p 80:5000 --name flask-devops -itd 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/rpao_webtest:latest
