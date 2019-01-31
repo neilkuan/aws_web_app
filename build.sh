18f7a5b6241a072b90c68c30761028ba0aa2bdf5
Neil Guan <46012524+guan840912@users.noreply.github.com>
2019-01-31 13:30:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -ex
 #set registry
 USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
-IMAGE=iii-devops-ecr
+IMAGE=rpao_webtest
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
